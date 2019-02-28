a694b33b903b71ad10e69a8e0a15382911571a36
Cameron Pavey <cpave3@gmail.com>
2019-02-28 19:15:35 +1000

    :sparkles: Added fancy tool tip

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ import WebFont from "webfontloader";
 
 WebFont.load({
   google: {
-    families: ['Muli:300,400,700', 'sans-serif']
-  }
+    families: ["Muli:300,400,700", "sans-serif"],
+  },
 });
 
 const Main = styled.article`
@@ -23,20 +23,62 @@ const Main = styled.article`
   flex: 1;
 `;
 
+const Floater = styled.div`
+  background-color: #3498db;
+  color: #ecf0f1;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-bottom: 20px;
+  position: relative;
+  font-weight: bold;
+  font-family: 'Muli', sans-serif;
+  &::after {
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-top: 10px solid #3498db;
+    position: absolute;
+    top: 95%;
+    left: 50%;
+    transform: translateX(-50%);
+    content: "";
+  }
+
+  animation: float 6s ease-in-out infinite;
+  @keyframes float {
+    0% {
+      /* box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6); */
+      transform: translatey(0px);
+    }
+    50% {
+      /* box-shadow: 0 25px 15px 0px rgba(0, 0, 0, 0.2); */
+      transform: translatey(-5px);
+    }
+    100% {
+      /* box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6); */
+      transform: translatey(0px);
+    }
+  }
+`;
+
 const generateOutput = (
   count: number,
   mode: string,
   multi: boolean,
-  setOutput: (value: string) => void
+  setOutput: (value: string) => void,
+  setInit: (value: boolean) => void,
 ) => {
   // First generate the right number of sentences
+  setInit(false);
   const sentences = [];
   const paraSize = 10;
   for (let i = 0; i < count; i++) {
-    sentences.push(i> 0 && i % paraSize === 0 ? '\n\r' : runOnce(multi));
+    sentences.push(i > 0 && i % paraSize === 0 ? "\n\r" : runOnce(multi));
   }
 
-  const stringified = sentences.reduce((acc, curr) => acc + curr, '');
+  const stringified = sentences.reduce((acc, curr) => acc + curr, "");
 
   setOutput(stringified);
 };
@@ -54,6 +96,9 @@ const GenerateButton = styled.button`
   background-color: #27ae60;
   color: white;
   border: none;
+  font-weight: bold;
+  font-family: 'Muli', sans-serif;
+  border-radius: 5px;
   &:hover {
     cursor: pointer;
     background-color: #2ecc71;
@@ -62,6 +107,7 @@ const GenerateButton = styled.button`
 
 const ContentArea = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   margin: 10px 0;
@@ -79,7 +125,7 @@ const TextArea = styled.textarea`
   flex: 1;
   border: 1px solid #bdc3c7;
   font-size: 1.15em;
-  font-family: 'Muli';
+  font-family: "Muli", sans-serif;
   box-sizing: border-box;
   min-width: 100%;
   max-width: 100%;
@@ -111,6 +157,7 @@ const App = () => {
   const [content, setContent] = useState("paragraph");
   const [count, setCount] = useState(20);
   const [output, setOutput] = useState("");
+  const [init, setInit] = useState(true);
   return (
     <Wrapper>
       <Header />
@@ -121,7 +168,7 @@ const App = () => {
             value={count}
             onChange={e => setCount(parseInt(e.target.value))}
           />
-          <span>&nbsp;{count === 1 ? 'sentence' : 'sentences'} of&nbsp;</span>
+          <span>&nbsp;{count === 1 ? "sentence" : "sentences"} of&nbsp;</span>
           <TextType
             handleChange={(value: boolean) => setMulti(value)}
             value={multi}
@@ -129,9 +176,10 @@ const App = () => {
         </Controls>
         <TextArea className="border flex-1" value={output} />
         <ContentArea>
+          { init ? <Floater>Click here to get started!</Floater> : null }
           <GenerateButton
             onClick={() => {
-              generateOutput(count, content, multi, setOutput);
+              generateOutput(count, content, multi, setOutput, setInit);
             }}
           >
             Generate
