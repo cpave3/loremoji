f1f53d7d65914b410e3bfd98b1fec2ff647446e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-21 02:20:16 +0000

    Deploy cpave3/loremoji to github.com/cpave3/loremoji.git:gh-pages

---
+++ precache-manifest.4216a9ede251e6caac5c60208dcfddaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fea7648923292124d18a",
+    "url": "/static/css/main.7458c2f3.chunk.css"
+  },
+  {
+    "revision": "fea7648923292124d18a",
+    "url": "/static/js/main.fea76489.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "c7315f2f99a3a2bcbacd",
+    "url": "/static/js/2.c7315f2f.chunk.js"
+  },
+  {
+    "revision": "0d2a8b78f790079fb00f09250d3a7a15",
+    "url": "/index.html"
+  }
+];
